730adda15a54affd661d61528eb1a0ee64097144
Romain <leconteromain.lr@gmail.com>
2019-07-22 22:33:27 +0200

    connect to mongoDB

--- serv/app.js
+++ serv/app.js
@@ -1,13 +1,29 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const mongoose = require("mongoose");
 const api = require("../serv/api/v1/index");
-
+const connection = mongoose.connection;
 app.set("port", process.env.port || 3000);
 
 app.use(cors());
 app.use("/api/v1", api);
+app.use((req, res) => {
+  const err = new Error("404 - Not Found");
+  err.status = 404;
+  res.json({ msg: "404 | not found", err: err });
+});
 
-app.listen(app.get("port"), () => {
-  console.log(`express listening port ${app.get("port")}`);
+mongoose.connect("mongodb://localhost:27017/expressangul", {
+  useNewUrlParser: true
+});
+connection.on("error", err => {
+  console.error(`connection to mongodb error ${err.message}`);
+});
+
+connection.once("open", () => {
+  console.log("connected to mongoDB");
+  app.listen(app.get("port"), () => {
+    console.log(`express listening port ${app.get("port")}`);
+  });
 });
