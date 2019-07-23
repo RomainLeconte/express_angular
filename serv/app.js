e8b817e0fac3b29f6a45b5d7a9ddd44928ab1525
Romain <leconteromain.lr@gmail.com>
2019-07-23 12:31:45 +0200

    add api

--- serv/app.js
+++ serv/app.js
@@ -3,9 +3,12 @@ const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
 const api = require("../serv/api/v1/index");
+const bodyParser = require("body-parser");
 const connection = mongoose.connection;
 app.set("port", process.env.port || 3000);
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 app.use("/api/v1", api);
 app.use((req, res) => {
